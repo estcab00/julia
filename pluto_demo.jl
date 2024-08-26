eaba25fc8b2b72e95ecf91c50401049a76ccb935
Esteban Cabrera <esteban.cabrera@pucp.edu.pe>
2024-08-26 12:23:43 -0500

    update

---
+++ pluto_demo.jl
@@ -0,0 +1,55 @@
+### A Pluto.jl notebook ###
+# v0.19.45
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0e513d80-63cf-11ef-1c5a-416dd97a2cd2
+println("Hello, World!")
+
+# ╔═╡ db04e9ea-9ecc-4acf-99ad-c70b80b02ea3
+println("Hello, World!")
+
+# ╔═╡ 0b47a18b-9500-4d67-ba1f-5fbf84b318e0
+println("Hello, World!")
+
+# ╔═╡ b1f6e16b-b5a5-40c9-88bb-d0a268f9a927
+# This is a comment
+
+# ╔═╡ 06aba240-0fa0-4366-b5a2-8643775377e9
+md"# This is a header"
+
+# ╔═╡ cad84c9b-316f-47d3-81b3-0fe575229786
+md"##### This is a header"
+
+# ╔═╡ cd515dd4-568e-4d38-bd1c-619cfe549939
+md"This is markdown text"
+
+# ╔═╡ 3e421baf-ca3a-4c0d-8949-529c68d3a350
+x = 2
+
+# ╔═╡ 100ee48a-7b3b-4721-af8b-9290664df209
+y = 2
+
+# ╔═╡ 2c10e1d7-c201-45a6-aa34-f09bfa376ebc
+z = x + y
+
+# ╔═╡ d360bae2-574b-4c9e-bb65-fadc9563a807
+
+
+# ╔═╡ 77dcaa75-8458-47c3-853c-b1caa0171da5
+
+
+# ╔═╡ Cell order:
+# ╠═0e513d80-63cf-11ef-1c5a-416dd97a2cd2
+# ╠═db04e9ea-9ecc-4acf-99ad-c70b80b02ea3
+# ╠═0b47a18b-9500-4d67-ba1f-5fbf84b318e0
+# ╟─b1f6e16b-b5a5-40c9-88bb-d0a268f9a927
+# ╟─06aba240-0fa0-4366-b5a2-8643775377e9
+# ╟─cad84c9b-316f-47d3-81b3-0fe575229786
+# ╟─cd515dd4-568e-4d38-bd1c-619cfe549939
+# ╠═3e421baf-ca3a-4c0d-8949-529c68d3a350
+# ╠═100ee48a-7b3b-4721-af8b-9290664df209
+# ╠═2c10e1d7-c201-45a6-aa34-f09bfa376ebc
+# ╠═d360bae2-574b-4c9e-bb65-fadc9563a807
+# ╠═77dcaa75-8458-47c3-853c-b1caa0171da5
